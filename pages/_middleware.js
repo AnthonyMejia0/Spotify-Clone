3e517d564553200f716cceec4b85983eec875aa4
AnthonyMejia0 <mejia.anthony96@gmail.com>
2022-06-16 18:02:55 -0700

    added middleware

---
+++ pages/_middleware.js
@@ -0,0 +1,24 @@
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+
+export async function middleware(req) {
+    const url = req.nextUrl.clone();
+    url.pathname = '/login';
+
+    // Token exists if user logged in
+    const token = await getToken({ req, secret: process.env.JWT_SECRET });
+
+    const { pathname } = req.nextUrl;
+
+    // Allow requests if the following are true
+    // 1. request for next-auth session & provider fetching
+    // 2. token exists
+    if (pathname.includes("/api/auth") || token) {
+        return NextResponse.next();
+    }
+
+    // Redirect to login if no token & requesting protected route 
+    if (!token && pathname !== "/login") {
+        return NextResponse.redirect(url);
+    }
+}
