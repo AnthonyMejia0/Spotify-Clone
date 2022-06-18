9e79c4db50364997474e6c48a2d463099f64243a
AnthonyMejia0 <mejia.anthony96@gmail.com>
2022-06-17 19:31:42 -0700

    created song and song components

--- components/Songs.js
+++ components/Songs.js
@@ -1,6 +1,20 @@
+import { useRecoilValue } from "recoil";
+import { playlistState } from "../atoms/playlistAtom";
+import Song from "./Song";
+
 function Songs() {
+  const playlist = useRecoilValue(playlistState);
+  
   return (
-    <div className="text-white">Songs</div>
+    <div className="flex flex-col px-8 pb-28 space-y-1 text-white">
+      {playlist?.tracks?.items.map((track, i) => (
+        <Song 
+          key={track.track.id}
+          order={i}
+          track={track}
+        />
+      ))}
+    </div>
   )
 }
 
