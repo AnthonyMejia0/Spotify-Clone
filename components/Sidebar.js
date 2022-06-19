f3edda6cfb60a0a2f3193e24fe2a96d224bfdd75
AnthonyMejia0 <mejia.anthony96@gmail.com>
2022-06-19 04:15:57 -0700

    improved responsiveness

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,5 +1,5 @@
-import { HeartIcon, HomeIcon, LibraryIcon, LogoutIcon, PlusCircleIcon, RssIcon, SearchIcon } from '@heroicons/react/outline'
-import { signOut, useSession } from 'next-auth/react'
+import { HeartIcon, HomeIcon, LibraryIcon, LogoutIcon, PlusCircleIcon, RssIcon, SearchIcon } from '@heroicons/react/outline';
+import { signOut, useSession } from 'next-auth/react';
 import { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import { playlistIdState } from '../atoms/playlistAtom';
@@ -20,10 +20,10 @@ function Sidebar() {
   }, [session, spotifyApi]);
 
   return (
-    <div className='text-gray-500 p-5 text-xs lg:text-sm border-r-gray-900 border-r overflow-y-scroll scrollbar-hide h-screen sm:min-w-[12rem] lg:min-w-[15rem] hidden md:inline-flex'>
+    <div className='text-gray-500 p-5 text-xs lg:text-sm border-r-gray-900 border-r overflow-y-scroll scrollbar-hide h-[calc(100vh-6rem)] sm:min-w-[12rem] lg:min-w-[15rem] hidden md:inline-flex pb-36'>
       <div className='space-y-4'>
-      <button className='flex items-center space-x-2 hover:text-white'
-      onClick={() => signOut()}>
+        <button className='flex items-center space-x-2 hover:text-white'
+        onClick={() => signOut()}>
           <LogoutIcon className='h-5 w-5'/>
           <p className=''>Log Out</p>
         </button>
@@ -40,7 +40,7 @@ function Sidebar() {
           <p className=''>Your Library</p>
         </button>
 
-        <hr className='border-t-[0.1px] border-gray-900'/>
+        <hr className='border-t-[0.1px] border-gray-900' />
 
         <button className='flex items-center space-x-2 hover:text-white'>
           <PlusCircleIcon className='h-5 w-5'/>
