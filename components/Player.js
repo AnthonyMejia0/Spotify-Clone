d1d4f2c1564d30be7760684fc899fb4a38686bf7
AnthonyMejia0 <mejia.anthony96@gmail.com>
2022-06-22 15:56:50 -0700

    added song seeking control

--- components/Player.js
+++ components/Player.js
@@ -1,5 +1,8 @@
 import { SwitchHorizontalIcon, FastForwardIcon, ReplyIcon, RewindIcon, PlayIcon, PauseIcon, VolumeUpIcon, VolumeOffIcon } from "@heroicons/react/solid";
-import { debounce } from "lodash";
+import RepeatIcon from '@mui/icons-material/Repeat';
+import RepeatOneIcon from '@mui/icons-material/RepeatOne';
+import Slider from '@mui/material/Slider';
+import { debounce, max } from "lodash";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useCallback, useEffect, useState } from "react";
@@ -7,6 +10,7 @@ import { useRecoilState } from "recoil";
 import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
 import useSongInfo from "../hooks/useSongInfo";
 import useSpotify from "../hooks/useSpotify";
+import { millisToMinutesAndSeconds } from "../lib/time";
 
 
 function Player() {
@@ -15,13 +19,21 @@ function Player() {
     const [currentTrackId, setCurrentTrackId] = useRecoilState(currentTrackIdState);
     const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
     const [volume, setVolume] = useState(100);
+    const [seek, setSeek] = useState(Number(0));
+    const [timer, setTimer] = useState(Number(0));
+    const [isDragging, setIsDragging] = useState(false);
     const [isShuffle, setIsShuffle] = useState(false);
-    const [isRepeat, setIsRepeat] = useState(false);
+    const [isRepeat, setIsRepeat] = useState("off");
     const songInfo = useSongInfo();
-    const router = useRouter();
 
     function delay(time) {
         return new Promise(resolve => setTimeout(resolve, time));
+    }
+
+    function add1sec() {
+        if (isPlaying) {
+            setTimer(timer + 1);
+        }
     }
 
     const fetchCurrentSong = () => {
@@ -33,11 +45,15 @@ function Player() {
                 spotifyApi.getMyCurrentPlaybackState().then((data) => {
                     setIsPlaying(data.body?.is_playing);
                     setIsShuffle(data.body?.shuffle_state);
+                    //console.log("THIS >>>", data.body?.progress_ms);
                     if (data.body?.repeat_state === "off") {
-                        setIsRepeat(false);
+                        setIsRepeat("off");
+                    }
+                    else if (data.body?.repeat_state === "context") {
+                        setIsRepeat("context");
                     }
                     else {
-                        setIsRepeat(true);
+                        setIsRepeat("track");
                     }
                 });
             });
@@ -72,13 +88,17 @@ function Player() {
 
     const handleRepeat = () => {
         spotifyApi.getMyCurrentPlaybackState().then((data) => {
-            if (data.body.repeat_state === "track" || data.body.repeat_state === "context") {
+            if (data.body.repeat_state === "track") {
                 spotifyApi.setRepeat("off");
-                setIsRepeat(false);
+                setIsRepeat("off");
+            }
+            else if (data.body.repeat_state === "context") {
+                spotifyApi.setRepeat("track");
+                setIsRepeat("track");
             }
             else {
                 spotifyApi.setRepeat("context");
-                setIsRepeat(true);
+                setIsRepeat("context");
             }
         });
     };
@@ -94,9 +114,20 @@ function Player() {
     useEffect(() => {
         if (spotifyApi.getAccessToken() && !currentTrackId) {
             fetchCurrentSong();
-            setVolume(100);
+            setVolume(100);           
         }
     }, [currentTrackIdState, spotifyApi, session]);
+
+    useEffect(() => {
+        //setTimeout(() => {
+            spotifyApi.getMyCurrentPlayingTrack().then((data) => {
+                if (!isDragging) {
+                    setSeek(data.body.progress_ms);
+                    add1sec();
+                }
+            }).catch((err) => {err});
+        //}, 1000);
+    }, [timer, isPlaying])
     
     useEffect(() => {
         if (volume > 0 && volume < 100) {
@@ -107,6 +138,13 @@ function Player() {
     const debouncedAdjustVolume = useCallback(
         debounce((volume) => {
             spotifyApi.setVolume(volume).catch((err) => {});
+        }, 500),
+        []
+    );
+
+    const debouncedSeek = useCallback(
+        debounce((seek) => {
+            spotifyApi.seek(seek).catch((err) => {});
         }, 500),
         []
     );
@@ -122,47 +160,106 @@ function Player() {
                 />
                 <div className="overflow-x-hidden">
                     <h3 className="text-blue-500 truncate">{songInfo?.name}</h3>
-                    <p className="text-gray-500 truncate">{getArtists()}</p>
+                    <p className="text-gray-500 truncate pr-2">{getArtists()}</p>
                 </div>
             </div>
 
             {/* Middle */}
-            <div className="flex items-center justify-around md:justify-evenly text-gray-500">
-                <SwitchHorizontalIcon onClick={handleShuffle} className={`bigButton md:button ${isShuffle ? 'text-[#1DB954]' : 'text-gray-500'}`} />
-                <RewindIcon 
-                    onClick={() => {
-                        spotifyApi.skipToPrevious();
-                        delay(3000).then(() => {
-                            let nextTrack = spotifyApi.getMyCurrentPlayingTrack();
-                            nextTrack.then((data) => {
-                                setCurrentTrackId(data?.body?.item.id);
+            <div className="flex flex-col justify-center">
+                <div className="flex items-center justify-around md:justify-evenly text-gray-500">
+                    <SwitchHorizontalIcon onClick={handleShuffle} className={`bigButton md:button ${isShuffle ? 'text-[#1DB954]' : 'text-gray-500'}`} />
+                    <RewindIcon 
+                        onClick={() => {
+                            spotifyApi.skipToPrevious();
+                            setSeek(2500);
+                            delay(2500).then(() => {
+                                let nextTrack = spotifyApi.getMyCurrentPlayingTrack();
+                                nextTrack.then((data) => {
+                                    setCurrentTrackId(data?.body?.item.id);
+                                });
                             });
-                        });
-                    }} 
-                    className="bigButton md:button" />
-                
-                {isPlaying ? (
-                    <PauseIcon
-                        onClick={handlePlayPause}
-                        className="button w-10 h-10 text-white"
+                        }} 
+                        className="bigButton md:button" 
                     />
-                ) : (
-                    <PlayIcon
-                        onClick={handlePlayPause}
-                        className="button w-10 h-10 text-white"
-                    />
-                )}
+                    
+                    {isPlaying ? (
+                        <PauseIcon
+                            onClick={handlePlayPause}
+                            className="button w-10 h-10 text-white"
+                        />
+                    ) : (
+                        <PlayIcon
+                            onClick={handlePlayPause}
+                            className="button w-10 h-10 text-white"
+                        />
+                    )}
 
-                <FastForwardIcon onClick={() => {
-                    spotifyApi.skipToNext();
-                    delay(3000).then(() => {
-                        let nextTrack = spotifyApi.getMyCurrentPlayingTrack();
-                        nextTrack.then((data) => {
-                            setCurrentTrackId(data?.body?.item.id);
-                        });
-                    });
-                }} className="bigButton md:button" />
-                <ReplyIcon onClick={handleRepeat} className={`button ${isRepeat ? 'text-[#1DB954]' : 'text-gray-500'}`} />
+                    <FastForwardIcon 
+                        onClick={() => {
+                            spotifyApi.skipToNext();
+                            setSeek(2500);
+                            delay(2500).then(() => {
+                                let nextTrack = spotifyApi.getMyCurrentPlayingTrack();
+                                nextTrack.then((data) => {
+                                    setCurrentTrackId(data?.body?.item.id);
+                                });
+                            });
+                        }} 
+                        className="bigButton md:button"
+                    />
+
+                    {isRepeat === "off" ? (
+                        <RepeatIcon onClick={handleRepeat} className="button text-gray-500"/>
+                    ) : (
+                        isRepeat === "context" ? (
+                            <RepeatIcon onClick={handleRepeat} className="button text-[#1DB954]" />
+                        ) : (
+                            <RepeatOneIcon onClick={handleRepeat} className="button text-[#1DB954]" />
+                        )
+                    )}
+                </div>
+                <div className="flex space-x-2 items-center">
+                <p className="text-white">{millisToMinutesAndSeconds(seek)}</p>
+                    <Slider 
+                        aria-label="time-indicator"
+                        size="small"
+                        value={seek}
+                        min={0}
+                        step={1}
+                        max={songInfo?.duration_ms}
+                        onChange={(_, value) => {
+                            setIsDragging(true);
+                            setSeek(Number(value));
+                        }}
+                        onChangeCommitted={(_, value) => {
+                            spotifyApi.seek(seek).catch((err) => {});
+                            setIsDragging(false);
+                            setTimer(timer+1);
+                        }}
+                        sx={{
+                            color:'white',
+                            height: 4,
+                            '&:hover': {
+                                color: '#1DB954',
+                            },
+                            '& .MuiSlider-thumb': {
+                              width: 0,
+                              height: 0,
+                              transition: '0.3s cubic-bezier(.47,1.64,.41,.8)',
+                              '&:hover, &.Mui-focusVisible': {
+                                width: 15,
+                                height: 15,
+                                color: 'white',
+                              },
+                            },
+                            '& .MuiSlider-rail': {
+                              color: 'white',
+                              opacity: 0.28,
+                            },
+                          }}
+                    />
+                    <p className="text-white">{millisToMinutesAndSeconds(songInfo?.duration_ms)}</p>
+                </div>
             </div>
 
             {/* Right */}
