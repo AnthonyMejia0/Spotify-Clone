2fbd1bf20ea7d52056eab14f0dfb7f0582438ffc
AnthonyMejia0 <mejia.anthony96@gmail.com>
2022-06-23 00:01:39 -0700

    added mobile nav

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,4 +1,5 @@
 import { HeartIcon, HomeIcon, LibraryIcon, LogoutIcon, PlusCircleIcon, RssIcon, SearchIcon } from '@heroicons/react/outline';
+import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
 import { signOut, useSession } from 'next-auth/react';
 import { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
@@ -20,52 +21,74 @@ function Sidebar() {
   }, [session, spotifyApi]);
 
   return (
-    <div className='text-gray-500 p-5 text-xs lg:text-sm border-r-gray-900 border-r overflow-y-scroll scrollbar-hide h-[calc(100vh-6rem)] sm:min-w-[12rem] lg:min-w-[15rem] hidden md:inline-flex pb-36'>
-      <div className='space-y-4'>
-        <button className='flex items-center space-x-2 hover:text-white'
-        onClick={() => signOut()}>
-          <LogoutIcon className='h-5 w-5'/>
-          <p className=''>Log Out</p>
-        </button>
-        <button className='flex items-center space-x-2 hover:text-white'>
-          <HomeIcon className='h-5 w-5'/>
-          <p className=''>Home</p>
-        </button>
-        <button className='flex items-center space-x-2 hover:text-white'>
-          <SearchIcon className='h-5 w-5'/>
-          <p className=''>Search</p>
-        </button>
-        <button className='flex items-center space-x-2 hover:text-white'>
-          <LibraryIcon className='h-5 w-5'/>
-          <p className=''>Your Library</p>
-        </button>
+    <div>
+      <div className='text-gray-500 p-5 text-xs lg:text-sm border-r-gray-900 border-r overflow-y-scroll scrollbar-hide h-[calc(100vh-6rem)] sm:min-w-[12rem] lg:min-w-[15rem] hidden md:inline-flex pb-36'>
+        <div className='space-y-4'>
+          <button className='flex items-center space-x-2 hover:text-white'
+          onClick={() => signOut()}>
+            <LogoutIcon className='h-5 w-5'/>
+            <p className=''>Log Out</p>
+          </button>
+          <button className='flex items-center space-x-2 hover:text-white'>
+            <HomeIcon className='h-5 w-5'/>
+            <p className=''>Home</p>
+          </button>
+          <button className='flex items-center space-x-2 hover:text-white'>
+            <SearchIcon className='h-5 w-5'/>
+            <p className=''>Search</p>
+          </button>
+          <button className='flex items-center space-x-2 hover:text-white'>
+            <LibraryIcon className='h-5 w-5'/>
+            <p className=''>Your Library</p>
+          </button>
 
-        <hr className='border-t-[0.1px] border-gray-900' />
+          <hr className='border-t-[0.1px] border-gray-900' />
 
-        <button className='flex items-center space-x-2 hover:text-white'>
-          <PlusCircleIcon className='h-5 w-5'/>
-          <p className=''>Create Playlist</p>
-        </button>
-        <button className='flex items-center space-x-2 hover:text-white'>
-          <HeartIcon className='h-5 w-5'/>
-          <p className=''>Liked Songs</p>
-        </button>
-        <button className='flex items-center space-x-2 hover:text-white'>
-          <RssIcon className='h-5 w-5'/>
-          <p className=''>Your Episodes</p>
-        </button>
+          <button className='flex items-center space-x-2 hover:text-white'>
+            <PlusCircleIcon className='h-5 w-5'/>
+            <p className=''>Create Playlist</p>
+          </button>
+          <button className='flex items-center space-x-2 hover:text-white'>
+            <HeartIcon className='h-5 w-5'/>
+            <p className=''>Liked Songs</p>
+          </button>
+          <button className='flex items-center space-x-2 hover:text-white'>
+            <RssIcon className='h-5 w-5'/>
+            <p className=''>Your Episodes</p>
+          </button>
 
-        <hr className='border-t-[0.1px] border-gray-900'/>
+          <hr className='border-t-[0.1px] border-gray-900'/>
 
-        {playlists.map((playlist) => (
-          <p
-            key={playlist.id} 
-            onClick={() => setPlaylistId(playlist.id)}
-            className='cursor-pointer hover:text-white'
-          >
-            {playlist.name}
-          </p>
-        ))}
+          {playlists.map((playlist) => (
+            <p
+              key={playlist.id} 
+              onClick={() => setPlaylistId(playlist.id)}
+              className='cursor-pointer hover:text-white'
+            >
+              {playlist.name}
+            </p>
+          ))}
+        </div>
+      </div>
+      <div className='flex items-center justify-around absolute bottom-0 h-[4.5rem] w-full p-1 overflow-hidden bg-black opacity-90 md:hidden'>
+        <button className='mobile-menu'>
+          <div className='flex flex-col items-center'>
+            <HomeIcon className='h-8 w-10 text-white' />
+            <p className='text-white'>Home</p>
+          </div>
+        </button>
+        <button className='mobile-menu'>   
+          <div className='flex flex-col items-center'>
+            <SearchIcon className='h-8 w-10 text-white' />
+            <p className='text-white'>Search</p>
+          </div>
+        </button>
+        <button className='mobile-menu'>
+          <div className='flex flex-col items-center'>
+            <LibraryMusicIcon className='h-8 w-10 text-white' />
+            <p className='text-white'>Library</p>
+          </div>
+        </button>
       </div>
     </div>
   )
