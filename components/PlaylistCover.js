a606864832cecc55b8e57cfc896141c1899cc2df
AnthonyMejia0 <mejia.anthony96@gmail.com>
2022-06-26 16:33:28 -0700

    added support for playlist display

---
+++ components/PlaylistCover.js
@@ -0,0 +1,25 @@
+import { useRecoilState } from "recoil"
+import { pageState } from "../atoms/pageAtom";
+import { playlistIdState } from "../atoms/playlistAtom"
+
+function PlaylistCover({ image, title, owner, songs, id }) {
+  const [playlistId, setPlaylistId] = useRecoilState(playlistIdState);
+  const [pageName, setPageName] = useRecoilState(pageState);
+
+  return (
+      <div 
+        onClick={() => {
+          setPlaylistId(id);
+          setPageName("Center");
+        }}
+        className="max-w-[8rem] md:max-w-[10rem] bg-[#181818] hover:bg-zinc-800 p-4 rounded-md text-sm cursor-pointer"
+      >
+        <img className="h-24 min-w-[6rem] md:h-32 md:min-w-[8rem] mb-2" src={image} alt="" />
+        <p className="font-bold truncate">{title}</p>
+        <p>{owner}</p>
+        <p>{songs > 1 ? `${songs} Songs` : `${songs} Song`}</p>
+      </div>
+  )
+}
+
+export default PlaylistCover
