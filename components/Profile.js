d2f59e82665392cb659ade74d4617a9e835f5b8b
AnthonyMejia0 <mejia.anthony96@gmail.com>
2022-06-29 03:26:44 -0700

    created profile component

--- components/Profile.js
+++ components/Profile.js
@@ -1,10 +1,14 @@
 import { ChevronDownIcon } from "@heroicons/react/outline"
-import { useSession } from "next-auth/react"
+import { signOut, useSession } from "next-auth/react"
 
 function Profile() {
-    const { data: session } = useSession();
+  const { data: session } = useSession();
+
   return (
-    <div className="w-[12.5rem] absolute flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2">
+    <div 
+      onClick={() => signOut()}
+      className="w-[12.5rem] absolute flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2"
+    >
         <img 
             className="rounded-full w-10 h-10" 
             src={session?.user.image} 
